4c488dd7ef90f081f14460d10f6c3695fe694d29
rfdornelles <rodornelles@gmail.com>
2020-12-01 19:07:05 -0300

    Função para buscar incidente documentada e melhorada a regex

--- R/2-pegar_incidentes.R
+++ R/2-pegar_incidentes.R
@@ -1,60 +1,42 @@
-library(magrittr)
+##### Monitor do Controle Concentrado no STF  ######
+### Rodrigo Dornelles
+### dezembro/2020
 
-# Import -----------------------------------------------------------------------
-
-u_stf_listar <- "http://portal.stf.jus.br/processos/listarProcessos.asp"
-
-classe <- "ADPF"
-numero <- "760"
-
-q_stf_listar <- list("classe" = classe,
-                     "numeroProcesso" = numero)
-
-r_stf_listar <- httr::GET(u_stf_listar,
-                          query = q_stf_listar,
-                          httr::write_disk("Teste_listar_ADPF.html", T))
-
-# identificar o incidente, vou precisar pra tudo
-
-incidente <- r_stf_listar$url %>%
-  stringr::str_extract("[0-9]+$")
-
-q_incidente <- list("incidente" = incidente)
-
-###
+## Função auxiliar para obter o número de incidente de cada caso que irei
+# utilizar
 
 library(magrittr)
 
-stf_busca_incidente <- function (classe, numero, dorme = 0, prog) {
 
-  if (!missing(prog)) prog()
+stf_busca_incidente <- function (classe, numero, dorme = 1, prog) {
 
+# se a barra de progresso for informada, chamar a função
+  if (!missing(prog)) {
+    prog()
+
+  }
+
+# dormir X segundos para não sobrecarregar o Tribunal
   Sys.sleep(dorme)
 
+# url da busca de processos do STF
   u_stf_listar <- "http://portal.stf.jus.br/processos/listarProcessos.asp"
 
+# query que busca classe e numero
   q_stf_listar <- list("classe" = classe,
                        "numeroProcesso" = numero)
 
+# requisição. não vou salvar pois não me interessa manter esse arquivo
   r_stf_listar <- httr::GET(u_stf_listar,
                             query = q_stf_listar)
 
+
+# vou buscar na resposta o URL para o qual fui redirecionado, ele conterá
+# o numero do incidente na query
+# retornar os números existentes ao final da expressão, que vai con
+
   r_stf_listar$url %>%
-    stringr::str_extract("[0-9]+$")
+         stringr::str_extract("(?<=\\?incidente\\=)[0-9]+$")
 
 }
-
-base_teste <- casos_interesse %>% head()
-
-progressr::with_progress({
-
-  p <- progressr::progressor(nrow(base_teste))
-
-  base_teste %>%
-    dplyr::mutate(incidente =
-                    purrr::map2_chr(.x = classe, .y = numero,
-                                    .f = stf_busca_incidente, dorme = 1, prog = p)) %>%
-    dplyr::select(classe, numero, incidente)
-
-})
 
