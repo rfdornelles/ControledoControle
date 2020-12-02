b4a122190f50d0b8c08d548f4506ee2e71e9e2e0
rfdornelles <rodornelles@gmail.com>
2020-12-02 12:41:03 -0300

    Documentado e organizado o arquivo que monta as análises

--- R/7-Organizar-tudo.R
+++ R/7-Organizar-tudo.R
@@ -1,5 +1,21 @@
+##### Monitor do Controle Concentrado no STF  ######
+### Rodrigo Dornelles
+### dezembro/2020
+
+library(magrittr)
+
+## Aqui vamos carregar os scripts de 1 a 5 em que tenho as funções que utilizei
+# não vou chamar as funões de baixar pois as rodei em separado no script 6
+
+## Agora o objetivo é resgatar a lista de incidentes e rodar as funções de
+# leitura
+
+# Após, salvar tudo em arquivos .rds para não perder tempo no futuro
+
+# Feito tudo isso estarei em condições de realizar as análises
+
 ##### criar as funções
-tictoc::tic()
+
 source("R/1-baixar_lista_processos.R")
 source("R/2-pegar_incidentes.R")
 source("R/3-pegar_dados.R")
@@ -50,15 +66,25 @@ progressr::with_progress({
 
   tabela_peticoes <- lista_incidentes %>%
     dplyr::pull(incidente) %>%
-    purrr::map_df(ler_pdf_inicial, prog = p) %>%
-    dplyr::mutate(palavras_chave = retornar_palavras_frequentes(texto_inicial,))
+    purrr::map_df(
+      purrr::possibly(~ler_pdf_inicial(incidente = .x,
+                                       prog = p), otherwise = NULL)
+    )
 
 })
+
+## procurar palavras-chave
+
+tabela_palavras_chave <- tabela_peticoes %>%
+  dplyr::mutate(
+    palavras_chave = retornar_palavras_frequentes(texto_inicial)) %>%
+  dplyr::select(-texto_inicial)
 
 
 ## salvar
 
 readr::write_rds(tabela_andamentos, "data/andamentos.rds")
 readr::write_rds(tabela_partes, "data/partes.rds")
-readr::write_rds(tabela_peticoes, "data/peticoes.rds")
-tictoc::toc()
+readr::write_rds(tabela_peticoes, "data/peticoes.rds", compress = "gz")
+readr::write_rds(tabela_palavras_chave, "data/palavras-chave.rds")
+
