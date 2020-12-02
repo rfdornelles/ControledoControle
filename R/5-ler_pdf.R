e7913378b1d0d944fc2bbdc5cda2cf02c15f31ef
rfdornelles <rodornelles@gmail.com>
2020-12-02 00:20:25 -0300

    Função que retorna as palavras mais frequentes do documento

--- R/5-ler_pdf.R
+++ R/5-ler_pdf.R
@@ -27,11 +27,62 @@ Sys.sleep(dormir)
 
   pdf_lido <- pdftools::pdf_text(caminho_pet)
 
-  pdf_lido <- pdf_lido %>%
+  pdf_lido %>%
     stringr::str_c(collapse = "") %>%
     stringr::str_squish()
-
 }
 
 #### procurar palavras-chave
 # referência: https://www.ufrgs.br/wiki-r/index.php?title=Frequ%C3%AAncia_das_palavras_e_nuvem_de_palavras
+
+# estabelece as stopwords
+palavras_comuns <- c("art", "lei", "stf", "corte", "pgr", "º",
+                     "rj", "min", "ministro", "ministra", "tcm",
+                    "nao", "federal") %>%
+  tibble::tibble("word" = .) %>%
+  dplyr::full_join(., tidytext::get_stopwords("pt"))
+
+
+#### retornar_palavras_frequentes ####
+
+# Função que retorna as X palavras mais frequentes na inicial
+# útil para categorizar o assunto e criar wordclouds
+# no futuro pode ajudar a fazer modelo de tópicos
+
+retornar_palavras_frequentes <- function(texto, quantidade = 10,
+                                         simplifica = TRUE) {
+
+  # limpar o texto
+
+  texto_limpo <-  texto %>%
+    abjutils::rm_accent() %>% # remover acentsos
+    stringr::str_squish() %>% # Elimina espacos excedentes
+    stringr::str_to_lower() %>% # Converte para minusculo
+    tibble::tibble("texto" = .) # transforma em tbl
+
+  texto_limpo <- texto_limpo %>%
+    tidytext::unnest_tokens(input = texto, output = "token") %>%
+    dplyr::anti_join(., y = palavras_comuns,
+                     by = c("token" = "word")) %>%
+    dplyr::count(token, sort = T) %>%
+    dplyr::slice_head(n = quantidade)
+
+  # pra facilitar iteração retornar apenas as palavras
+  if(!simplifica) {
+
+    texto_limpo <- texto_limpo %>%
+      dplyr::pull(token)
+
+  }
+
+# retornar os termos no formato desejado
+  texto_limpo
+
+}
+
+
+
+
+
+
+
