3a1ca1b4f91f45ffbd467ee1410ed1cfb18cbc7b
rfdornelles <rodornelles@gmail.com>
2020-12-01 22:12:28 -0300

    Documentada a função que baixa os arquivos de andamentos e partes em disco

--- R/3-pegar_dados.R
+++ R/3-pegar_dados.R
@@ -1,70 +1,50 @@
-# partes
-
-u_partes <- "http://portal.stf.jus.br/processos/abaPartes.asp"
-
-r_partes <- httr::GET(url = u_partes,
-                      query = q_incidente)
-
-partes_tipo <- r_partes %>%
-  xml2::read_html() %>%
-  xml2::xml_find_all("//div[@class='detalhe-parte']") %>%
-  xml2::xml_text() %>%
-  stringr::str_extract("(?>[A-Z]*)")
-
-partes_nomes <- r_partes %>%
-  xml2::read_html() %>%
-  xml2::xml_find_all("//div[@class='nome-parte']") %>%
-  xml2::xml_text() %>%
-  stringr::str_replace_all("&nbsp", " ") %>%
-  stringr::str_squish()
-
-
-partes <- tibble::tibble(tipo = partes_tipo,
-                         nome = partes_nomes)
-
-
-# andamentos
-
-u_andamentos <- "http://portal.stf.jus.br/processos/abaAndamentos.asp"
-
-r_andamentos <- httr::GET(url = u_andamentos,
-                          query = q_incidente)
-
-
-####
+##### Monitor do Controle Concentrado no STF  ######
+### Rodrigo Dornelles
+### dezembro/2020
 
 library(magrittr)
 
+
+## As funções aqui vão servir para baixar os dados das partes e os andamentos
+# e para ler os arquivos baixados
+
+# garantir que existem as pastas
+
+fs::dir_create("data-raw/partes/")
+fs::dir_create("data-raw/andamentos/")
+
 baixar_dados_processo <- function (incidente, dormir = 0, naMarra = F, prog) {
 
-  # barra de progresso
-  if (!missing(prog)) prog()
+# barra de progresso, para quando for iterar
+  if (!missing(prog)) {
+    prog()
+}
 
-  # sleep para não causar
+# sleep para não causar
   Sys.sleep(dormir)
 
-  # preparar a querry
+# preparar a querry colocando o incidente
 
   q_incidente <- list("incidente" = incidente)
 
-  # urls que serão buscadas
+# urls que serão buscadas
   u_partes <- "http://portal.stf.jus.br/processos/abaPartes.asp"
   u_andamentos <- "http://portal.stf.jus.br/processos/abaAndamentos.asp"
 
-  # futuros arquivos
+ # nomes dos futuros arquivos
   caminho_partes <- paste0("data-raw/partes/Partes-", incidente, ".html")
 
   caminho_andamentos <- paste0("data-raw/andamentos/Andamentos-", incidente,
                                ".html")
 
-  # baixar partes se não existir e se não precisar forçar
+# baixar partes se não existir e se não precisar forçar
 
   if(!file.exists(caminho_partes) | naMarra) {
     httr::GET(url = u_partes, query = q_incidente,
               httr::write_disk(caminho_partes, overwrite = TRUE))
   }
 
-  # baixar andamentos se não existir e se não precisar forçar
+# baixar andamentos se não existir e se não precisar forçar
   if(!file.exists(caminho_andamentos) | naMarra) {
     httr::GET(url = u_andamentos, query = q_incidente,
               httr::write_disk(caminho_andamentos, overwrite = TRUE))
@@ -74,15 +54,55 @@ baixar_dados_processo <- function (incidente, dormir = 0, naMarra = F, prog) {
 }
 
 
-### teste
-
-progressr::with_progress({
-
-  p <- progressr::progressor(nrow(base_incidentes))
-
-  base_incidentes %>%
-    dplyr::pull(incidente) %>%
-    purrr::walk(., .f = baixar_dados_processo, dormir = 1, prog = p)
-
-})
+# ### ler os dados
+#
+# # partes
+#
+# u_partes <- "http://portal.stf.jus.br/processos/abaPartes.asp"
+#
+# r_partes <- httr::GET(url = u_partes,
+#                       query = q_incidente)
+#
+# partes_tipo <- r_partes %>%
+#   xml2::read_html() %>%
+#   xml2::xml_find_all("//div[@class='detalhe-parte']") %>%
+#   xml2::xml_text() %>%
+#   stringr::str_extract("(?>[A-Z]*)")
+#
+# partes_nomes <- r_partes %>%
+#   xml2::read_html() %>%
+#   xml2::xml_find_all("//div[@class='nome-parte']") %>%
+#   xml2::xml_text() %>%
+#   stringr::str_replace_all("&nbsp", " ") %>%
+#   stringr::str_squish()
+#
+#
+# partes <- tibble::tibble(tipo = partes_tipo,
+#                          nome = partes_nomes)
+#
+#
+# # andamentos
+#
+# u_andamentos <- "http://portal.stf.jus.br/processos/abaAndamentos.asp"
+#
+# r_andamentos <- httr::GET(url = u_andamentos,
+#                           query = q_incidente)
+#
+#
+# ####
+#
+#
+#
+#
+# ### teste
+#
+# progressr::with_progress({
+#
+#   p <- progressr::progressor(nrow(base_incidentes))
+#
+#   base_incidentes %>%
+#     dplyr::pull(incidente) %>%
+#     purrr::walk(., .f = baixar_dados_processo, dormir = 1, prog = p)
+#
+# })
 
