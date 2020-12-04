2e5c852dd6df69b6455539f7e70458d92b837ea5
rfdornelles <rodornelles@gmail.com>
2020-12-04 19:32:58 -0300

    Correções nos gráficos de proponentes

--- R/8-analises.R
+++ R/8-analises.R
@@ -12,22 +12,31 @@ library(stringr)
 
 source("R/1-baixar_lista_processos.R")
 
+#### carregar as bases do site do STF
+
 base_simplificada <- ler_tabela_stf()
+
+# carregar os incidentes
 lista_incidentes <- readr::read_rds("data/incidentes.rds")
 
+# juntar os incidentes à base
 base_referencia <- base_simplificada %>%
   dplyr::left_join(lista_incidentes)
 
-## grafico distribuição de açõees
+#### grafico distribuição de açõees
 
+## ver distribuição ao longo dos anos
 grafico_distribuicao_acoes <- base_simplificada %>%
   dplyr::count(ano) %>%
   ggplot(aes(x = ano, y = n)) +
   geom_line(size = 2) +
   theme_classic() +
   labs(x = "Ano", y = "Processos distribuídos",
-       title = "Distribuição de ações do STF")
+       title = "Distribuição de ações no STF") +
+  scale_x_binned(n.breaks = 20)
 
+
+## ver distribuição por classe processual
 grafico_acoes_classe <- base_simplificada %>%
   ggplot(mapping = aes(x = ano, fill = classe)) +
   geom_bar() +
@@ -35,12 +44,16 @@ grafico_acoes_classe <- base_simplificada %>%
   xlab("Ano") +
   ylab("Classe processual") +
   labs(title = "Distribuição das ações",
-       subtitle = "Controle concentrado ao longo dos anos")
+       subtitle = "Controle concentrado ao longo dos anos") +
+  scale_x_binned(limits = c(2000, 2020), n.breaks = 20)
 
 
-## princiáis partidos
-
+##### principais partidos
+# carregar partes
 base_partes <- readr::read_rds(file = "data/partes.rds")
+
+### categorizar as partes por seu nome
+# após limpar os nomes, vamos criar categorias
 
 base_partes_categorizada <- base_partes %>%
   dplyr::filter(tipo == "REQTE") %>%
@@ -48,36 +61,59 @@ base_partes_categorizada <- base_partes %>%
     nome = abjutils::rm_accent(nome),
     nome = stringr::str_to_lower(nome),
     nome = stringr::str_squish(nome),
-    nome = stringr::str_remove_all(nome, "e outro\\(a\\/s\\)")) %>%
+    nome = stringr::str_remove_all(nome, "e outro\\(a\\/s\\)"),
+    nome = stringr::str_remove_all(nome, "[[:punct:]]")
+    ) %>%
   dplyr::mutate(
     categoria = dplyr::case_when(
-      str_detect(nome, "presidente da republica") ~ "Presidente da República",
-      str_detect(nome, "geral da republica") ~ "PGR",
+      str_detect(nome, "^presidente da republica") ~ "Presidente da República",
+      str_detect(nome, "geral da republica$") ~ "PGR",
       str_detect(nome, "(procurador|procuradora).geral da repub.ica") ~ "PGR",
-      str_detect(nome, "partido") ~ "Partido Político",
+      str_detect(nome, "^partido") ~ "Partido Político",
       str_detect(nome, "associacao|confederacao|federacao|sindicato|sindical") ~ "OSC",
       str_detect(nome, "uniao nacional|uniao geral|articulacao|central nacional") ~ "OSC",
       str_detect(nome, "ordem dos advogados") ~ "OAB",
-      str_detect(nome, "governador*|prefeit*") ~ "Governador/Prefeito",
-      str_detect(nome, "estado de *") ~ "Governador/Prefeito",
+      str_detect(nome, "^governador*|prefeit*") ~ "Governador/Prefeito",
+      str_detect(nome, "^estado de *") ~ "Governador/Prefeito",
       str_detect(nome, "rede sustentabilidade|podemos|solidariedade|cidadania|democratas - diretorio nacional") ~ "Partido Político",
       TRUE ~ NA_character_
     )) %>% select(-tipo)
 
-# quais partidos
+## join para obter ano
+base_partes_categorizada <- base_partes_categorizada %>%
+  dplyr::left_join(base_referencia)
+
+
+## ver quando essas categorias propuseram as ações
+grafico_distribuicao_2020_categoria <- base_partes_categorizada %>%
+  filter(ano == 2020, !is.na(categoria)) %>%
+  group_by(categoria, autuacao) %>%
+  count(autuacao) %>%
+  ggplot(aes(x = autuacao, y = n, color = categoria)) +
+  geom_line(size = 1.1, show.legend = F) +
+  scale_y_continuous(limits = c(1, 15), n.breaks = 15, name = NULL) +
+  theme_classic() +
+  facet_wrap(~categoria) +
+  labs(title = "Distribuição de ações em 2020",
+       subtitle = "Por categoria de proponente", caption = "Até 04/12/2020")
+
+
+### ver quais partidos são os que mais acionam
 
 tabela_quais_partidos <- base_partes_categorizada %>%
   filter(categoria == "Partido Político") %>%
-  mutate(nome = str_to_title(nome)) %>%
+  mutate(nome = str_to_upper(nome),
+         nome = str_squish(nome)) %>%
   count(nome, sort = T) %>%
   head(10) %>%
   knitr::kable()
 
-# quais OSC
+### quais OSC
 
 tabela_quais_osc <- base_partes_categorizada %>%
   filter(categoria == "OSC") %>%
-  mutate(nome = str_to_title(nome)) %>%
+  mutate(nome = str_to_upper(nome),
+         nome = str_squish(nome)) %>%
   count(nome, sort = T) %>%
   head(10) %>%
   knitr::kable()
@@ -85,7 +121,7 @@ tabela_quais_osc <- base_partes_categorizada %>%
 
 base_partes_categorizada <- base_partes_categorizada %>%
   left_join(base_referencia) %>%
-  filter(!is.na(categoria), ano >= 2016, ano <= 2020)
+  filter(!is.na(categoria))
 
 
 grafico_partes_categorizadas <- base_partes_categorizada %>%
@@ -99,8 +135,7 @@ grafico_partes_categorizadas <- base_partes_categorizada %>%
        title = "Evolução da distribuição de ações",
        subtitle = "Por categoria de proponentes")
 
-
-#
+#######################
 
 base_temas <- base_partes_categorizada %>%
   select(-autuacao, -distribuicao, -polo_ativo, -polo_passivo, -dia) %>%
@@ -169,7 +204,7 @@ base_partes_amicus %>%
          nome = abjutils::rm_accent(nome)) %>%
   count(nome, sort = T) %>% View()
 
-)
+
 
 
 
